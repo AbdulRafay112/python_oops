e263fa035844a446b33487a36e19815368ababb1
AbdulRafay112 <rafayrafay922@gmail.com>
2025-09-04 17:10:26 +0500

    added in range

--- lab_assignments/lab_8/Range/Range.py
+++ lab_assignments/lab_8/Range/Range.py
@@ -1,71 +1,75 @@
 class Range:
-    """Our Own Range class that mimics range in python
-    Our Range class is different in two ways from python range class 
-    1- it has a default constructor means if you donot provide any argument its ok for Range
-    2- it has copy constructor logic 
-    """
-    def __init__(self , start = 0 , stop = None , step = 1): # default constructor
-        """Initialize the Range class constructor
-        Args:
-            start : integer you wants to start the sequence 
-            stop: integer you wants to end the sequence but it always ends number-1
-            step: integer you want to step your sequence 
-        """
-        if isinstance(start , Range) :
-            # copy constructor logic
-            self._length = start._length
-            self._start = start._start 
-            self._step = start._step 
-            self._stop = start._stop 
-        else:
-            self._step_validation(step)
-            self._type_validation(step)
-            self._type_validation(start)
-            if stop is not None :
-                self._type_validation(stop)
-            if stop is None :
-                # handling one argument case 
-                stop = start 
-                start = 0 
-            # protected instance variables 
-            self._length = self._calculate_length(start , stop , step)
-            self._start = start 
-            self._step = step 
-            self._stop = stop     
+    """Simple Range class that mimics the behavior of built in range class of python"""
+    def __init__(self , start: int , stop: int = None , step: int = 1):
+        """ initialize a range
+         Args: 
+             start: start of the range 
+             stop: stop of the range its usually stop (n-1) pattern
+             step: its steps the sequence of the range 
+           """
+        if stop is None:# checking for 1 argument case e.g-> Range(4)
+            stop = start 
+            start = 0 
+        self._type_validation(start , stop , step) # type validation
+        self._step_validation(step) # step zero case checking 
+        self._length = self._calculate_length(start , stop , step) # effective length of the range
+        self._start = start 
+        self._stop = stop 
+        self._step = step 
 
-    # === dunder methods === 
-    def __len__(self):
-        """Return length of the range"""
+    # === dunder methods ===
+    def __len__(self) -> int:
+        """Return number of entries in the range"""
         return self._length
-    def __getitem__(self , j):
-        """Return entry at index k use standar interpretation if j < 0 which support negative indexing"""
-        self._validate_index(j)
-        if j<0:
-            j+=len(self)
-        return self._start + j * self._step 
-    def __str__(self):
-        """String representation of range"""
-        return f"start is {self._start} , stop is {self._stop} , step is {self._step}"
+    def __getitem__(self , k: int) -> int :
+        k = self._negative_index_handling(k) # index negative case handling 
+        self._index_validation(k)
+        return self._start + k * self._step
+    
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+    def __repr__(self) -> str :
+        """Developer representation"""
+        return f"Range({self._start} , {self._stop} , {self._step})"
+    def __str__(self) -> str :
+        """client representation"""
+        return f"start = {self._start} , stop = {self._stop} , step = {self._step}"
+    def __eq__(self , other) -> None :
+        """Compare with other Range or built in range"""
+        if isinstance(other , Range) or isinstance(other , range):
+            print(list(self))
+            print(list(other))
+            return (list(self) == list(other))
+        return False 
 
-    # === helper methods ===
 
-    def _validate_index(self , j):
-        if j >= len(self):
-            raise IndexError("index is exceed")
-        if not isinstance(j , int):
-            raise TypeError("index should be int type")
-        
-    @staticmethod
-    def _step_validation(step):
+
+     # === helper functions ===       
+         
+    def _step_validation(self , step: int)->None:
+        """If step is equals to zero raise value error"""
         if step == 0:
             raise ValueError("step can not be zero")
-    @staticmethod
-    def _type_validation(other):
-        if not isinstance(other , int):
-            raise TypeError("Start , stop , step must be integer type")
-    @staticmethod
-    def _calculate_length(start , stop , step):
-        """Calculate length of the range"""
-        return max(0, (stop - start + step - 1) // step)
+    def _calculate_length(self , start: int , stop: int , step: int)->int:
+        """Calculate the effective length of the range"""
+        if (step > 0 and start >= stop) or (step < 0 and start <= stop):
+            return 0
+        return ((abs(stop - start) - 1) // abs(step)) + 1
+        
+    def _type_validation(self , start: int , stop: int , step: int) -> None :
+        """Type validation of all three: start , stop , step"""
+        for name , val in {"start": start , "stop": stop , "step": step}.items():
+            if not isinstance(val , int):
+                raise TypeError(f"{name} must be int type")
+    def _negative_index_handling(self , k: int)->int:
+        """if index is negative then increment to its length and return it"""
+        return k + len(self) if k < 0 else k 
+    def _index_validation(self , k: int) -> None:
+        """Index validation and index type checking"""
+        if not isinstance(k , int):
+            raise TypeError("index must be int type")
+        if not 0 <= k < len(self):
+            raise IndexError("index out of range")
 
-
+    
--- lab_assignments/lab_8/Range/main.py
+++ lab_assignments/lab_8/Range/main.py
@@ -1,19 +1,13 @@
 from Range import Range 
 if __name__ == "__main__":
     try:
-        # implement default constructor logic 
-        r1 = Range(5)
-        # implement copy constructor logic
-        r2 = Range(r1)
-        # implementing parameterized constructor case 
-        r3 = Range(3 , 10 , 4)
-        # implementing the __getitem__ dunder method 
-        print(r2[4])
-        # implementing the __len__ method 
-        print(len(r3))
-    except Exception as e :
+        r1 = Range(4 , 10 , 2)
+        r2 = range(4 , 10 ,2)
+        for i in r1:
+            print(i)
+        print("-" * 23)
+        for i in r2:
+            print(i)
+    
+    except Exception as e:
         print(e)
-
-
-
-
